ce2bdfe5b838bac7a370d7bf323013c8d54349fe
skupadhyay024 <connect.shashik@gmail.com>
2018-12-17 08:41:34 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+
+
+
+
+
+const routes: Routes = [ { path: "", redirectTo: "home", pathMatch: 'full' },
+      { path: "home", component: HomeComponent },
+  { path: "employees", component: EmployeeListComponent }]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+     
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { MaterialModule } from '@angular/material';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    EmployeeListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
